9099c95f80f83f51b5d0f9fc5b6a216fdcd0567e
Vicky768 <roysudeepto168@gmail.com>
2021-03-05 12:36:14 +0530

    Initial structure committed to main

---
+++ src/test/java/com/store/ecommerce/Ecommerce_Store/EcommerceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.ecommerce.Ecommerce_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
